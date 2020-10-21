a78f97545e4a4dbc3fbb2ddc1099cfd21a7160e9
leiweibo <leiweibo@gmail.com>
2020-10-21 15:18:36 +0800

    remove the key crenditial

--- java/src/main/java/com/wblei/test/TestFireStoreServer.java
+++ java/src/main/java/com/wblei/test/TestFireStoreServer.java
@@ -7,6 +7,7 @@ import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -14,6 +15,7 @@ import com.google.firebase.cloud.FirestoreClient;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.concurrent.ExecutionException;
 
@@ -23,7 +25,7 @@ public class TestFireStoreServer {
       InterruptedException {
     // Use a service account
     //InputStream serviceAccount = TestFireStoreServer.class.getClassLoader().getResourceAsStream("playfirebase-58833-a0945921a733.json");
-    InputStream serviceAccount = TestFireStoreServer.class.getClassLoader().getResourceAsStream("mobilecleaner-8126e-firebase-adminsdk-cdzk3-cc34ec04e5.json");
+    InputStream serviceAccount = TestFireStoreServer.class.getClassLoader().getResourceAsStream("mobilecleaner-8126e-3445077f1b88.json");
 
     GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
     FirebaseOptions options = new FirebaseOptions.Builder()
@@ -47,24 +49,31 @@ public class TestFireStoreServer {
 
     CollectionReference users = db.collection("cookies");
     Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8"));
-    calendar.set(Calendar.DAY_OF_MONTH, 9);
-    calendar.set(Calendar.HOUR_OF_DAY, 16);
-    calendar.set(Calendar.MINUTE, 11);
-    calendar.set(Calendar.SECOND, 32);
+    calendar.set(Calendar.DAY_OF_MONTH, 20);
+    calendar.set(Calendar.HOUR_OF_DAY, 13);
+    calendar.set(Calendar.MINUTE, 37);
+    calendar.set(Calendar.SECOND, 0);
     calendar.set(Calendar.MILLISECOND, 0);
     Query query = users.whereGreaterThanOrEqualTo("timestamp", calendar.getTime());
 
     ApiFuture<QuerySnapshot> querySnapshot = query.get();
-
-
-    Iterable<DocumentReference> documents = users.listDocuments();
-    int i = 0;
-    for (DocumentReference documentReference : documents) {
-      i ++;
-      CookieObject cookie = documentReference.get().get().toObject(CookieObject.class);
+    List<QueryDocumentSnapshot> documentSnapshots = querySnapshot.get().getDocuments();
+    for (QueryDocumentSnapshot documentSnapshot : documentSnapshots) {
+      CookieObject cookie = documentSnapshot.toObject(CookieObject.class);
       System.out.println(cookie);
     }
-    System.out.println("共计：" + i);
+    System.out.println("共计：" + querySnapshot.get().size());
+
+
+    System.out.println("************************************");
+    //Iterable<DocumentReference> documents = users.listDocuments();
+    //int i = 0;
+    //for (DocumentReference documentReference : documents) {
+    //  i ++;
+    //  CookieObject cookie = documentReference.get().get().toObject(CookieObject.class);
+    //  System.out.println(cookie);
+    //}
+    //System.out.println("共计：" + i);
 
     //for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
     //  CookieObject cookie = document.toObject(CookieObject.class);
